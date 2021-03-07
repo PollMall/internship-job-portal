87d04b43737e7c1349815947af8f382cc7fbfe5f
Paul Popa <paulpopa@Paul-Popa-C02VW5BAG8WN.local>
2021-03-07 21:38:52 +0200

    fix: more test cases for Home component

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 import { render } from './setupTests';
 import Home from './Home';
 
-test('renders the route menu', () => {
-  render(<Router><Home /></Router>);
+describe('Home tests', () => {
+  it('renders the route menu', () => {
+    render(<Router><Home /></Router>);
+  });
+
+  it('should display main page', () => {
+    render(<Home />, undefined, { user: { username: 'user', password: 'user' } });
+    expect(screen.getByText(/welcome/i)).toBeInTheDocument();
+  });
 });
