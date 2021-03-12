5ead5d235c7776d897b46947a866ede450805ecb
Paul Popa <paulpopa@Paul-Popa-C02VW5BAG8WN.local>
2021-03-12 23:27:32 +0200

    fix: refactored data for JobDetailsSection

--- src/job-feed/JobDetails.js
+++ src/job-feed/JobDetails.js
@@ -4,9 +4,11 @@ import {
 } from '@material-ui/core';
 import JobDetailsSection from './JobDetailsSection';
 import useStyles from './useStyles';
+import makeSectionFields from './helper';
 
 function JobDetails({ job }) {
   const classes = useStyles();
+  const fields = makeSectionFields(job);
 
   return (
     <Card data-testid="job-details-data">
@@ -22,51 +24,7 @@ function JobDetails({ job }) {
         </div>
 
         <div className={classes.content}>
-          <JobDetailsSection
-            fields={[
-              {
-                title: 'Description',
-                data: [job.description],
-                component: 'p',
-              },
-              {
-                title: 'Contact info',
-                data: [
-                  `${job.company.user.firstName} ${job.company.user.lastName}`,
-                  `Address: ${job.company.contactInfocity}, ${job.company.contactInfo.country.name}`,
-                  `Email: ${job.company.contactInfo.email}`,
-                  `Phone: ${job.company.contactInfo.phone}`,
-                  `Website: ${job.company.contactInfo.website}`,
-                ],
-                component: 'p',
-              },
-              {
-                title: 'About company',
-                data: [job.company.contactInfo.about],
-                component: 'p',
-              },
-            ]}
-          />
-
-          <JobDetailsSection
-            fields={[
-              {
-                title: 'Required skills',
-                data: job.jobSkills.map((js) => js.skill.name),
-                component: 'li',
-              },
-              {
-                title: 'Job requirements',
-                data: job.jobRequirements.map((jr) => jr.name),
-                component: 'li',
-              },
-              {
-                title: 'Job benefits',
-                data: job.jobBenefits.map((jb) => jb.name),
-                component: 'li',
-              },
-            ]}
-          />
+          {fields.map((f) => <JobDetailsSection fields={f} />)}
         </div>
       </CardContent>
       <CardActions>
